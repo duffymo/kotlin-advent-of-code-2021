9d0bf596ceb182be98181c8dc2eead3a0b465dfe
michaelduffy <duffymo@gmail.com>
2021-12-04 16:06:42 -0500

    Duffy - Day 1 of Kotlin Advent of Code.  Success!

--- src/Utils.kt
+++ src/Utils.kt
@@ -8,6 +8,11 @@ import java.security.MessageDigest
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
 /**
+ * Read lines from the given input .txt file as a List of Int
+ */
+fun readInputAsInts(name: String) = File("src", "$name.txt").readLines().map { it.toInt() }
+
+/**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
